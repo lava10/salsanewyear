7afe005bd3b97eaef4e61f8af035c92f5523d740
Laura Valiente <lauramvaliente@gmail.com>
2018-02-12 21:27:02 -0800

    Added views and basic styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
+var app = express();
 var router = express.Router();
+var path = __dirname + '/views/';
 
-/* GET home page. */
+router.use(function (req, res, next) {
+  console.log("/" + req.method);
+  next();
+});
+
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
+app.use("/", router);
+
 module.exports = router;
+
